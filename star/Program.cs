b1c3da1a50712fe828f8e0dbd55428f762f984a6
PedramKamrani <kamranipedram@gmail.com>
2023-07-06 22:10:40 +0330

    Add project files.

---
+++ star/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("*");
+	for (int j = 0; j < 4; j++)
+	{
+        if (j%2==0)
+        {
+            Console.Write("+");
+        }
+        else
+        {
+            Console.Write("*");
+        }
+		
+      
+	}
+    Console.WriteLine("");
+}
+
+
+int number;
+Console.WriteLine("Please Enter Number");
+ number=int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Even Number");
+}
+else
+{
+    Console.WriteLine("Odd Number");
+}
